c6a365f06b5685f3f176a9f9943bf699af46329d
Nguyen Huy Vuong <vuongnguyenhuy@yahoo.com>
2018-04-14 16:04:54 -0500

    Glider updated

--- index.php
+++ index.php
@@ -3,10 +3,9 @@
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-<<<<<<< HEAD
-=======
+
     <link rel="stylesheet" href="style.css">
->>>>>>> d0c536f72ca21f0c33007e25997be7409752f1f6
+
     <title>Flight Animation</title>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
