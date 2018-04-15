97ac3de390f6d740224d8b271c2d0c479809ec35
DocQuantum <shchurgood@gmail.com>
2018-04-15 15:21:56 -0500

    Modified update script to show output to webpage

--- update.php
+++ update.php
@@ -1,7 +1,10 @@
 <?php
 
 shell_exec('cd /home/ugrad/dshchur/public_html/');
-shell_exec('git init');
-shell_exec('git pull');
+$output = shell_exec('git init');
+echo $output;
+echo "\r\n";
+$output = shell_exec('git pull 2>&1');
+echo $output;
 
 ?>
