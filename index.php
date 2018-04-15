780b897ece15f004c8f0a2d5ce8ec3822263034d
DocQuantum <shchurgood@gmail.com>
2018-04-15 10:29:00 -0500

    jquery test

--- index.php
+++ index.php
@@ -222,7 +222,7 @@
           });
           map.addLayer(flightsLayer); */
   </script>
-  <script>
+  <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous">
     var slider = document.getElementById("yearRange");
     var output = document.getElementById("yearValue");
     console.log(slider.value);
