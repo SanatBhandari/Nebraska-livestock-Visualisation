e2be3a1e8fbf66b6ffb6e48c68a12cd244848817
Nguyen Huy Vuong <vuongnguyenhuy@yahoo.com>
2018-04-14 19:20:51 -0500

    Update

--- index.php
+++ index.php
@@ -18,8 +18,8 @@
     <style>
       #overlay {
         position: fixed; /* Sit on top of the page content */
-        width: 100%; /* Full width (cover the whole page) */
-        height: 100%; /* Full height (cover the whole page) */
+        width: 1400px; /* Full width (cover the whole page) */
+        height: 700px; /* Full height (cover the whole page) */
         top: 0;
         left: 0;
         right: 0;
