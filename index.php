d6aeb7ae239bbcc7915b8c0dcb7f7a3557d86fb1
Nguyen Huy Vuong <vuongnguyenhuy@yahoo.com>
2018-04-14 19:30:42 -0500

    Updated

--- index.php
+++ index.php
@@ -18,12 +18,12 @@
     <style>
       #overlay {
         position: fixed; /* Sit on top of the page content */
-        width: 2500px; /* Full width (cover the whole page) */
+        width: 200%; /* Full width (cover the whole page) */
         height: 700px; /* Full height (cover the whole page) */
-        top: 200;
-        left: 100;
-        right: 100;
-        bottom: 200;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
         z-index: 2; /* Specify a stack order in case you're using a different order for other elements */
         cursor: pointer; /* Add a pointer on hover */
       }
