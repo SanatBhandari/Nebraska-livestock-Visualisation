9a64b071e5faccda57f7e9bff8f6c79ee670d99e
DocQuantum <shchurgood@gmail.com>
2018-04-14 23:53:50 -0500

    working on menu

--- index.php
+++ index.php
@@ -3,19 +3,13 @@
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-
     <link rel="stylesheet" href="style.css">
-
-    <title>Nebraska Livestock Visualisation</title>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://openlayers.org/en/v4.6.5/css/ol.css" type="text/css">
     <!-- The line below is only needed for old environments like Internet Explorer and Android 4.x -->
     <script src="https://cdn.polyfill.io/v2/polyfill.min.js?features=requestAnimationFrame,Element.prototype.classList,URL"></script>
     <script src="https://openlayers.org/en/v4.6.5/build/ol.js"></script>
     <script src="https://api.mapbox.com/mapbox.js/plugins/arc.js/v0.1.0/arc.js"></script>
+	<title>Nebraska Livestock Visualisation</title>
 </head>
 
 <body>
@@ -27,8 +21,7 @@
 </div>
 	<div id="dataNav" class="overlay">
 	<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-	<div class="panel-group overlay-content">
-        <div class="panel panel-default">
+	<div class="overlay-content">
             <form id="lstock" action="Database.php" method="get">
                 <div class="checkbox">
                             <label>
@@ -100,19 +93,18 @@
 						<output for="foo" onforminput="value = foo.valueAsNumber;"></output> -->
                         <tr>
                             <td>Year </td>
-                            <td><input id="myRange" type="range" min="2002" max="2017" value="2002" color="black" /></td>
+                            <td><input id="myRange" type="range" min="2002" max="2017" value="2002" color="white" /></td>
                             <td><output foo="foo" on forminput="value = foo.valueAsNumber;" /> </td>
                         </tr>
                     </div>
                         <button type="submit" class="btn btn-default" onclick="sendYear()">Submit</button>
                     </form>
-            </div>
         </div>
     </div>
 	
 	<script>
 	function openNav() {
-		document.getElementById("dataNav").style.width = "30%";
+		document.getElementById("dataNav").style.width = "40%";
 	}
 
 	function closeNav() {
